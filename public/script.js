75e2bf004d9b14e0a9a644ef0025a3929df707b4
Joao Cabral <joaocabral6665@gmail.com>
2020-07-07 21:46:35 -0400

    new site clone

--- public/script.js
+++ public/script.js
@@ -2,3 +2,35 @@ function menuToggler(buttonToggler){
    var menuToggler = document.getElementById("menu-toggle")
    menuToggler.classList.toggle("active")
 }
+
+// $('.carousel').slick({
+//    dots: true,
+//    infinite: true,
+//    speed: 500,
+//    slidesToShow: 4,
+//    slidesToScroll: 4,
+//  });
+
+$(document).ready(function(){
+   $('.carousel').slick({
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+   });
+});
+
+$(document).ready(function(){
+   $('.carousel-commercial-appeals').slick({
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      autoplay: true,
+      autoplaySpeed: 2000,
+   });
+ });
