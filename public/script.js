7f55f90dd253ae1c57d095827c353ff3ac657e7a
Joao Cabral <joaocabral6665@gmail.com>
2020-07-12 16:24:51 -0400

    final code clone printset

--- public/script.js
+++ public/script.js
@@ -24,13 +24,54 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-   $('.carousel-commercial-appeals').slick({
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 2,
-      slidesToScroll: 2,
-      autoplay: true,
-      autoplaySpeed: 2000,
-   });
+   var size = $(window).width();
+   
+   if(size <= 425){
+      $('.carousel-commercial-appeals').slick({
+         dots: false,
+         infinite: true,
+         speed: 500,
+         slidesToShow: 2,
+         slidesToScroll: 2,
+         autoplay: true,
+         autoplaySpeed: 2000,
+      });
+   }
+ });
+
+ $(document).ready(function(){
+   var size = $(window).width();
+   
+   if(size >=  768){
+      $('.carousel-company-brands').slick({
+         dots: false,
+         infinite: true,
+         speed: 500,
+         slidesToShow: 5,
+         slidesToScroll: 5,
+         autoplay: true,
+         autoplaySpeed: 2000,
+      });
+   }else if(size <=  768 && size > 425 ){
+      $('.carousel-company-brands').slick({
+         dots: false,
+         infinite: true,
+         speed: 500,
+         slidesToShow: 4,
+         slidesToScroll: 4,
+         autoplay: true,
+         autoplaySpeed: 2000,
+      });
+   }else if(size <=  425){
+      $('.carousel-company-brands').slick({
+         dots: false,
+         infinite: true,
+         speed: 500,
+         slidesToShow: 2,
+         slidesToScroll: 2,
+         autoplay: true,
+         autoplaySpeed: 2000,
+      });
+   }
+
  });
